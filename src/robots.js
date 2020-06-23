0d66ecaf39d2849405bf085a0f48336abe13508d
Vivika-I <63826609+Vivika-I@users.noreply.github.com>
2020-06-23 18:23:32 -0500

    Add files via upload

---
+++ src/robots.js
@@ -0,0 +1,65 @@
+export const robots=[
+	{
+		id:1,
+	name:'Jane Doe',
+	email:'Jane.Doe@gmail.com',
+   },
+
+{
+	id :2,
+	name:'lucillia rose',
+	email:'lucillia.rose@gmail.com',
+
+},
+{
+	id :3,
+	name:'Timothy Chabalgu',
+	email:'Timothy.Chabalgu@gmail.com',
+
+},
+{
+	id :4,
+	name:'Camilla ',
+	email:'Camilla@gmail.com',
+
+},
+{
+	id :5,
+	name:'Karen',
+	email:'Karen@gmail.com',
+
+},
+{
+	id :6,
+	name:'crystal Rose ',
+	email:'Crystal.Rose@gmail.com',
+
+},
+{
+	id :7,
+	name:' Oliver',
+	email:'Oliver@gmail.com',
+
+},
+{
+	id :8,
+	name:'james Peach',
+	email:'James.Peach@gmail.com',
+
+},
+{
+	id :9,
+	name:'Agustus Gloop',
+	email:'Agustus.Gloop@gmail.com',
+
+},
+{
+	id :10,
+	name:'fezco',
+	email:'fezco@gmail.com',
+
+}
+]
+export default robots;
+
+
